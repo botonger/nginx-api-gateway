c9701f363f3f80988b3e0c4e112f5f887d14cb1b
botonger <shinecheyenne@gmail.com>
2022-10-25 21:32:33 +0900

    MODIFY: api 응답수정

--- simple-rest/main.go
+++ simple-rest/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
 	"time"
 
@@ -13,14 +14,16 @@ func main() {
 	router := mux.NewRouter()
 
 	router.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-		writer.Write([]byte(fmt.Sprintf("Hello %s from %s\n", "anonymous", request.Host)))
+		writer.Write([]byte(fmt.Sprintf("Hello anonymous from %s\n", request.Host)))
 	}).Methods("GET")
 
-	router.HandleFunc("/{some:[-a-zA-z_0-9.]+}", func(writer http.ResponseWriter, request *http.Request) {
+	router.HandleFunc("/hello/{some:[-a-zA-z_0-9.]+}", func(writer http.ResponseWriter, request *http.Request) {
 		v := mux.Vars(request)
 		some := v["some"]
 
-		writer.Write([]byte(fmt.Sprintf("Hello %s from %s\n", some, request.Host)))
+		countries := []string{"Kysrgizstan", "India", "Congo", "Germany", "Bolivia"}
+
+		writer.Write([]byte(fmt.Sprintf("Hello %s from %s: %s\n", some, countries[rand.Intn(5)], request.Host)))
 	}).Methods("GET")
 
 	port := 8080
